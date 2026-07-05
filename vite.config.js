fa31116d1aedd306c89c5aea70b5e37252684dc4
D_037 <d.leo037.study@gmail.com>
2026-07-05 11:44:12 +0700

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import axios from "axios";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), axios()],
+	plugins: [react(), tailwindcss()],
 });
